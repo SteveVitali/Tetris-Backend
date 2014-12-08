74134c02bfc08c447a23b9481d332ebcb7a18824
Steve Vitali <14stevevitali@gmail.com>
2014-12-08 14:45:06 -0500

    Change url scheme

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 var api = require('./api');
 
 module.exports = function(app) {
-    app.post('/sprint/new', api.postSprintScore);
+    app.post('/sprint/scores/new', api.postSprintScore);
     app.get('/sprint/scores', api.getSprintScores);
-    app.get('/sprint/post-dummy', api.postDummyScore);
+    app.get('/sprint/scores/new-dummy-score', api.postDummyScore);
 }
